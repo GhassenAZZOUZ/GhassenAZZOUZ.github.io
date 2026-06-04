7f2c83b16071eeb0cb9f65e5006ad2befc855913
Ghassen Azzouz <ghassen_azzouz@ext.carrefour.com>
2026-06-04 13:35:28 +0200

    fix: detect Vercel env to set base path correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_PATH ?? '/monportfolio/',
+  base: process.env.VERCEL ? '/' : (process.env.BASE_PATH ?? '/monportfolio/'),
 })
